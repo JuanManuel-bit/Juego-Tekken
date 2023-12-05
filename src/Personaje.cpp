04107a76482dadf0a68184dcdbd0b74360534aff
JuanManuel-bit <145406028+JuanManuel-bit@users.noreply.github.com>
2023-12-04 22:24:03 -0600

    juego Tekken

---
+++ src/Personaje.cpp
@@ -0,0 +1,96 @@
+#include <SFML/Graphics.hpp>
+
+class Personaje
+{
+public:
+    Personaje(sf::Vector2f position, sf::Color color)
+    {
+        shape.setSize(sf::Vector2f(50, 50));
+        shape.setPosition(position);
+        shape.setFillColor(color);
+
+        if (!texture.loadFromFile("assets/images/LingXiaoyu.png"))
+        {
+        
+        }
+        this->sprite = sf::Sprite(texture);
+        this->sprite.setPosition(position);
+    }
+
+    void move(float offsetX, float offsetY)
+    {
+        sprite.move(offsetX, offsetY);
+        shape.move(offsetX, offsetY);
+    }
+
+    void draw(sf::RenderWindow &window)
+    {
+        window.draw(this->shape);
+        window.draw(this->sprite);
+    }
+
+    void update(){
+        if (clock.getElapsedTime().asSeconds() >= frameTime)
+        {
+            currentFrame = (currentFrame + 1) % numFrames;
+            sprite.setTextureRect(sf::IntRect((currentFrame * 64)+17, 133, 64, 36));
+            clock.restart();
+        }
+    }
+
+private:
+    sf::RectangleShape shape;
+    sf::Sprite sprite;
+    sf::Texture texture;
+    sf::Clock clock;
+    float frameTime = 0.1f;
+    int currentFrame = 0;
+    int numFrames = 4;
+    int frameWidth = 32;
+    int frameHeight = 32;
+};
+
+double velocidad = 0.1;
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "JuegoTekken");
+
+    Personaje pj(sf::Vector2f(400, 300), sf::Color::Red);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            pj.move(velocidad * -1, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            pj.move(velocidad, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            pj.move(0, velocidad * -1);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            pj.move(0, velocidad);
+        }
+        pj.update();
+
+        window.clear();
+        pj.draw(window);
+        window.display();
+    }
+
+    return 0;
+}
---
+++ src/Sprite.cpp
@@ -0,0 +1,48 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Sprite Animado");
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("assets/images/LingXiaoyu.png"))
+    {
+        return -1;
+    }
+
+    sf::Sprite sprite(texture);
+    sprite.setPosition(400, 300);
+
+    sf::Clock clock;
+    float frameTime = 0.1f;
+    int currentFrame = 0;
+    int numFrames = 4;
+    int frameWidth = 32;
+    int frameHeight = 32;
+
+    while (window.isOpen())
+    {
+        // Procesar eventos
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        if (clock.getElapsedTime().asSeconds() >= frameTime)
+        {
+            currentFrame = (currentFrame + 1) % numFrames;
+            sprite.setTextureRect(sf::IntRect((currentFrame * 64)+17, 133, 64, 36));
+            clock.restart();
+        }
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
+
+    return 0;
+}
